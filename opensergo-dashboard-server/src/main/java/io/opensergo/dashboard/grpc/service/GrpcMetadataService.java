5b807b1eaadf98ea9b42656bd2b871adc0d5e66a
luyanbo <robert.lyb@alibaba-inc.com>
2022-03-30 11:50:53 +0800

    reportMetadata will create application automatically

--- opensergo-dashboard-server/src/main/java/io/opensergo/dashboard/grpc/service/GrpcMetadataService.java
+++ opensergo-dashboard-server/src/main/java/io/opensergo/dashboard/grpc/service/GrpcMetadataService.java
@@ -2,7 +2,9 @@ package io.opensergo.dashboard.grpc.service;
 
 import com.google.protobuf.util.JsonFormat;
 import io.grpc.stub.StreamObserver;
+import io.opensergo.dashboard.domain.application.ApplicationEntity;
 import io.opensergo.dashboard.domain.application.MetadataEntity;
+import io.opensergo.dashboard.repository.application.ApplicationRepository;
 import io.opensergo.dashboard.repository.application.MetadataRepository;
 import io.opensergo.proto.service_contract.v1.MetadataServiceGrpc;
 import io.opensergo.proto.service_contract.v1.ReportMetadataReply;
@@ -22,6 +24,9 @@ public class GrpcMetadataService extends MetadataServiceGrpc.MetadataServiceImpl
     @Autowired
     private MetadataRepository metadataRepository;
 
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
     @SneakyThrows
     @Override
     public void reportMetadata(ReportMetadataRequest request, StreamObserver<ReportMetadataReply> responseObserver) {
@@ -35,12 +40,29 @@ public class GrpcMetadataService extends MetadataServiceGrpc.MetadataServiceImpl
                 .sortingMapKeys()
                 .omittingInsignificantWhitespace();
         String metadata = jsonPrinter.print(request);
+
         log.info("reportMetadata request: {}", metadata);
+
+        createApplication(appName);
+        recordMetadata(appName, metadataEntity, metadata);
+
+        responseObserver.onNext(ReportMetadataReply.newBuilder().build());
+        responseObserver.onCompleted();
+    }
+
+    private void createApplication(String appName) {
+        ApplicationEntity app = applicationRepository.findByName(appName).orElseGet(() ->
+                ApplicationEntity.builder()
+                        .name(appName)
+                        .build()
+        );
+        applicationRepository.save(app);
+    }
+
+    private void recordMetadata(String appName, MetadataEntity metadataEntity, String metadata) {
         metadataEntity.setAppName(appName);
         metadataEntity.setMetadata(metadata);
         metadataEntity.setSha512(DigestUtils.sha512Hex(metadata));
         metadataRepository.save(metadataEntity);
-        responseObserver.onNext(ReportMetadataReply.newBuilder().build());
-        responseObserver.onCompleted();
     }
 }
